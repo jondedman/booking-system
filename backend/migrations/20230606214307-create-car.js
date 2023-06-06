6959735828d933b6365192cf25cf380c4d58f51b
Jon Dedman <jonathandedman@googlemail.com>
2023-06-06 22:48:13 +0100

    generate car model

---
+++ backend/migrations/20230606214307-create-car.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      make: {
+        type: Sequelize.STRING
+      },
+      registration: {
+        type: Sequelize.STRING
+      },
+      lastMot: {
+        type: Sequelize.DATE
+      },
+      colour: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cars');
+  }
+};
