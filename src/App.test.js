1f6b1e7433e6c372ed36d1da554ad44897adc4ed
Jon Dedman <jonathandedman@googlemail.com>
2023-06-14 18:54:49 +0100

    add react testing library to project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// changed syntax to make it compatible with jest
+const { render, screen } = require("@testing-library/react");
+const App = require("./App").default;
+
+test("renders learn react link", () => {
+	render(App());
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
