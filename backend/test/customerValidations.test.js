89a82924a8ebd1298c7671d25c982ffaba2db155
Jon Dedman <jonathandedman@googlemail.com>
2023-06-15 23:29:23 +0100

    run customer validations in jest - all passed

---
+++ backend/test/customerValidations.test.js
@@ -0,0 +1,85 @@
+const { Sequelize } = require("sequelize");
+const { Customer } = require("../models");
+
+// Create a Sequelize instance
+const sequelize = new Sequelize("rdab", "jondedman", null, {
+	dialect: "postgres", // Replace with your preferred database dialect
+	// Add other necessary options here
+});
+
+describe("Customer Model Validations", () => {
+	test("should require firstName field", async () => {
+		try {
+			// Create a customer without a firstName
+			await Customer.create({
+				lastName: "Doe",
+				email: "test@example.com",
+				mobileNumber: "1234567890",
+				address: "123 Street, City",
+				dateAdded: new Date(),
+			});
+		} catch (error) {
+			expect(error.name).toBe("SequelizeDatabaseError");
+		}
+	});
+
+	test("should require lastName field", async () => {
+		try {
+			// Create a customer without a lastName
+			await Customer.create({
+				firstName: "John",
+				email: "test@example.com",
+				mobileNumber: "1234567890",
+				address: "123 Street, City",
+				dateAdded: new Date(),
+			});
+		} catch (error) {
+			expect(error.name).toBe("SequelizeDatabaseError");
+		}
+	});
+
+	test("should require email field", async () => {
+		try {
+			// Create a customer without an email
+			await Customer.create({
+				firstName: "John",
+				lastName: "Doe",
+				mobileNumber: "1234567890",
+				address: "123 Street, City",
+				dateAdded: new Date(),
+			});
+		} catch (error) {
+			expect(error.name).toBe("SequelizeDatabaseError");
+		}
+	});
+
+	test("should require mobileNumber field", async () => {
+		try {
+			// Create a customer without a mobileNumber
+			await Customer.create({
+				firstName: "John",
+				lastName: "Doe",
+				email: "test@example.com",
+				address: "123 Street, City",
+				dateAdded: new Date(),
+			});
+		} catch (error) {
+			expect(error.name).toBe("SequelizeDatabaseError");
+		}
+	});
+
+	test("should require address field", async () => {
+		try {
+			// Create a customer without an address
+			await Customer.create({
+				firstName: "John",
+				lastName: "Doe",
+				email: "test@example.com",
+				mobileNumber: "1234567890",
+				dateAdded: new Date(),
+			});
+		} catch (error) {
+			expect(error.name).toBe("SequelizeDatabaseError");
+		}
+	});
+});
