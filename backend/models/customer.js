5a435ae1438873c38f3dd662cab03efeb52fede1
Jon Dedman <jonathandedman@googlemail.com>
2023-06-16 20:43:02 +0100

    update main

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
 			this.hasMany(models.Booking, { foreignKey: "customerId" });
 		}
 	}
+	//test
 	Customer.init(
 		{
 			firstName: DataTypes.STRING,
