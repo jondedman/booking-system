92c9ca702b068f895dc8d3a092c07680b8b9a1e1
Jon Dedman <jonathandedman@googlemail.com>
2023-07-10 22:23:50 +0100

    format date in bookings component using luxon

--- frontend/vite-app/src/components/BookingListContainer.jsx
+++ frontend/vite-app/src/components/BookingListContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { DateTime } from "luxon";
 
 function BookingList() {
 	const [bookings, setBookings] = useState([]);
@@ -23,13 +24,18 @@ function BookingList() {
 		}
 	};
 
+	const formatBookingDate = (dateString) => {
+		const bookingDate = DateTime.fromISO(dateString);
+		return bookingDate.toLocaleString(DateTime.DATETIME_MED); // Format the date as desired
+	};
+
 	return (
 		<div>
 			<h1>Bookings</h1>
 			<ul>
 				{bookings.map((booking) => (
 					<li key={booking.id} style={{ display: "flex" }}>
-						<span>Date: {booking.date}</span>
+						<span>Date: {formatBookingDate(booking.date)}</span>
 						{booking.Customer && (
 							<span style={{ marginLeft: "10px" }}>
 								Customer: {booking.Customer.lastName}
