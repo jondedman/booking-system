30d6e62dde2e9dc5758e4ad2b44bf73a00b63aa9
Jon Dedman <jonathandedman@googlemail.com>
2023-07-12 14:26:48 +0100

    divide calander view into workshop and mot. update mot to appear not as all day event

--- frontend/vite-app/src/components/BigCalendar.jsx
+++ frontend/vite-app/src/components/BigCalendar.jsx
@@ -1,3 +1,51 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { Calendar, Views, globalizeLocalizer } from "react-big-calendar";
+// import { DateTime } from "luxon";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
+// import globalize from "globalize";
+// import EventComponent from "./EventComponent";
+
+// const localizer = globalizeLocalizer(globalize);
+// // consider changing default view to week
+
+// const BigCalendar = ({ bookings }) => {
+// 	const transformedBookings = bookings.map((booking) => ({
+// 		start: new Date(booking.date),
+// 		end: new Date(booking.date),
+// 		allDay: true,
+// 		title: `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName} - Vehicle: ${booking.Vehicle.make} Reg: ${booking.Vehicle.registration} - Mot:${booking.mot} Repair:${booking.repair} Diagnostic:${booking.diagnostic}`,
+// 		tooltip: `Customer contact: ${booking.Customer.mobileNumber} Quote: £${booking.quote} - Parts: £${booking.parts} Labour: £${booking.labour} Booked by: ${booking.User.username} `, //displays on hover
+// 		// try to use resources to add a view for mot:
+// 		// resources: "mot",
+// 	}));
+
+// 	return (
+// 		<div className="myCustomHeight">
+// 			<Calendar
+// 				localizer={localizer}
+// 				events={transformedBookings}
+// 				startAccessor="start"
+// 				endAccessor="end"
+// 				defaultView={Views.WEEK} // changes default view to week. other options: day, month, agenda
+// 				components={{
+// 					event: EventComponent,
+// 				}}
+// 			/>
+// 		</div>
+// 	);
+// };
+
+// BigCalendar.propTypes = {
+// 	bookings: PropTypes.arrayOf(
+// 		PropTypes.shape({
+// 			date: PropTypes.string.isRequired,
+// 			// Include other required properties from your 'bookings' object
+// 		})
+// 	).isRequired,
+// };
+
+// export default BigCalendar;
 import React from "react";
 import PropTypes from "prop-types";
 import { Calendar, Views, globalizeLocalizer } from "react-big-calendar";
@@ -7,18 +55,26 @@ import globalize from "globalize";
 import EventComponent from "./EventComponent";
 
 const localizer = globalizeLocalizer(globalize);
-// consider changing default view to week
 
 const BigCalendar = ({ bookings }) => {
+	console.log(bookings);
 	const transformedBookings = bookings.map((booking) => ({
 		start: new Date(booking.date),
 		end: new Date(booking.date),
-		allDay: true,
-		title: `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName} - Vehicle: ${booking.Vehicle.make} Reg: ${booking.Vehicle.registration} - Mot:${booking.mot} Repair:${booking.repair} Diagnostic:${booking.diagnostic}`,
-		tooltip: `Customer contact: ${booking.Customer.mobileNumber} Quote: £${booking.quote} - Parts: £${booking.parts} Labour: £${booking.labour} Booked by: ${booking.User.username} `, //displays on hover
-		// try to use resources to add a view for mot:
-		// resources: "mot",
+		allDay: booking.mot ? false : true,
+		title: booking.mot
+			? `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName} | Type: ${booking.Vehicle.type} Make: ${booking.Vehicle.make} Reg: ${booking.Vehicle.registration} | Repair: ${booking.repair} Diagnostic:${booking.diagnostic}`
+			: `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName}`,
+		resourceId: booking.mot ? "mot" : "workshop",
+		tooltip: booking.mot
+			? `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName} - Vehicle: ${booking.Vehicle.make} Reg: ${booking.Vehicle.registration} - Mot:${booking.mot} Repair:${booking.repair} Diagnostic:${booking.diagnostic}`
+			: `Customer: ${booking.Customer.firstName} ${booking.Customer.lastName}`,
 	}));
+
+	const resources = [
+		{ id: "mot", title: "MOT" },
+		{ id: "workshop", title: "Workshop" },
+	];
 
 	return (
 		<div className="myCustomHeight">
@@ -27,7 +83,10 @@ const BigCalendar = ({ bookings }) => {
 				events={transformedBookings}
 				startAccessor="start"
 				endAccessor="end"
-				defaultView={Views.WEEK} // changes default view to week. other options: day, month, agenda
+				resources={resources}
+				resourceIdAccessor="id"
+				resourceTitleAccessor="title"
+				defaultView={Views.WEEK}
 				components={{
 					event: EventComponent,
 				}}
